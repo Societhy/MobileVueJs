e53672f7785c0b1a26b4f656fa1503e24d647a10
Tetraa <aureliengiudici@msn.com>
2016-11-09 16:47:51 +0000

    turtur debug

--- app/components/profil.vue
+++ app/components/profil.vue
@@ -7,12 +7,17 @@
               <img src="./img/arthurSalop.jpg" alt="" class="responsive-img">
             </div>
                 <div class="input-field col s4">
-                    <input id="first_name" type="text" class="validate" v-model="firstName" >
-                    <label for="last_name">First Name</label>
+                    <input name="first_name" id="first_name" type="text" class="validate" v-model="firstName" >
+                    <label for="first_name">First Name</label>
                 </div>
                 <div class="input-field col s4">
-                    <input id="last_name" type="text" class="validate" v-model="lastName" >
+                    <input name="last_name" id="last_name" type="text" class="validate" v-model="lastName" >
                     <label for="last_name">Last Name</label>
+                </div>
+                <div class="col s12">
+                    <a class="btn-floating btn-large green">
+                        <i class="large material-icons">mode_edit</i>
+                    </a>
                 </div>
             </div>
         </div>
@@ -20,25 +25,25 @@
         <div class="carousel carousel-slider center card-panel grey" data-indicators="true">
          <div class="carousel-item blue white-text" href="#one!">
             <div class="row">
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-panel blue lighten-5 z-depth-1">
                 <div class="col s12">
                     <h2>Personal information</h2>
                 </div>
                 <div class="col s12">
-                    <input id="email" type="text" class="validate white-text" v-model="email"
-                    disabled="disabled">
+                    <input name="email" id="email" type="text" class="validate white-text" v-model="email">
                     <label for="email" class="white-text">Email</label>
                 </div>
                 <div class="col s12">
-                    <input id="phone" type="text" class="validate white-text" v-model="phone"
-                    disabled="disabled">
+                    <input name ="phone" id="phone" type="text" class="validate white-text" v-model="phone">
                     <label for="phone" class="white-text">Phone</label>
                 </div>
                 <div class="col s12">
-                    <input id="day-of-birthone" type="date" class=" datepicker validate white-text" v-model="dayofBirth"
-                    disabled="disabled">
-                    <label for="day-of-birth" class="white-text">Day of birth</label>
+                    <input name ="day-of-birth" id="day-of-birth" type="text" class="validate white-text" v-model="dayOfBirth">
+                    <label for="day-of-birth" class="white-text">dayOfBirth</label>
                 </div>
-            </div>
+        </div>
+        </div>
         </div>
         <div class="carousel-item green white-text" href="#two!">
             <h2>Activity</h2>
@@ -69,6 +74,7 @@
                 lastName: "Ngo-van",
                 email: "aurthurNgo@gmail.vietnam",
                 phone: "6666666666",
+                date:"14/04/1995",
             }
         },
         mounted() {
